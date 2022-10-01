ea7764d074f43c90dadfd7513e7b1a2cc939b814
charlie890414 <charlie890414@gmail.com>
2022-10-01 23:51:42 +0800

    scrollbar work!

--- Assets/Scripts/PlanetManager.cs
+++ Assets/Scripts/PlanetManager.cs
@@ -1,24 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlanetManager : MonoBehaviour
 {
     public GameObject parent;
     public GameObject template;
+    public Scrollbar scrollbar;
+    public Button button;
+
+    private int iter;
+    private bool pause;
+
     // Start is called before the first frame update
     void Start()
     {
+        pause = false;
+        iter = 1;
         Application.targetFrameRate = 60;
         GameObject planet1 = Instantiate(template, new Vector3(100, 0, 0), Quaternion.identity, parent.transform);
         planet1.name = "Alzir";
         GameObject planet2 = Instantiate(template, new Vector3(-25, 0, 0), Quaternion.identity, parent.transform);
         planet2.name = "Antares";
+        scrollbar.onValueChanged.AddListener((float val) => ScrollbarCallback(val));
+        button.onClick.AddListener(() => buttomCallback());
     }
 
     // Update is called once per frame
     void Update()
     {
-        parent.transform.eulerAngles += new Vector3(0, (float)0.8, 0);
+        if (!pause)
+        {
+            parent.transform.eulerAngles = new Vector3(0, 1 * iter, 0);
+            scrollbar.value = (float)iter / 720;
+            iter %= 720;
+            iter++;
+        };
+    }
+
+    void ScrollbarCallback(float value)
+    {
+        if (pause)
+        {
+            iter = (int)(value * 720);
+            parent.transform.eulerAngles = new Vector3(0, 1 * iter, 0);
+        }
+    }
+    void buttomCallback()
+    {
+        pause = !pause;
     }
 }
