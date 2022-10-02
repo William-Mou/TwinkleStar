9db37054ad043b3c8ff5eea4899c72769fe179de
William-Mou <william.mou1024@gmail.com>
2022-10-02 11:11:59 +0800

    Add UI for CreateStarScene

--- Assets/Scripts/PlanetIntroManager.cs
+++ Assets/Scripts/PlanetIntroManager.cs
@@ -13,16 +13,26 @@ public class PlanetIntroManager : MonoBehaviour
 
     private int iter;
     private bool pause;
-
+    public int ratioA = 1;
+    public int ratioB = 2;
+    const int ratioBias = 25;
+    GameObject planet1;
+    GameObject planet2;
+    Vector3 centerPosition;
+    int preventRatioA = 1;
+    int preventRatioB = 2;
     // Start is called before the first frame update
     void Start()
     {
         pause = false;
         iter = 1;
         Application.targetFrameRate = 60;
-        GameObject planet1 = Instantiate(template, new Vector3(100, 0, 0), Quaternion.identity, parent.transform);
+        centerPosition = parent.transform.position;
+        Debug.Log(centerPosition);
+
+        planet1 = Instantiate(template, new Vector3(ratioA * ratioBias, 0, 0) + centerPosition, Quaternion.identity, parent.transform);
         planet1.name = "Alzir";
-        GameObject planet2 = Instantiate(template, new Vector3(-25, 0, 0), Quaternion.identity, parent.transform);
+        planet2 = Instantiate(template, new Vector3(-1 * ratioB * ratioBias, 0, 0) + centerPosition, Quaternion.identity, parent.transform);
         planet2.name = "Antares";
         scrollbar.onValueChanged.AddListener((float val) => ScrollbarCallback(val));
         pauseButton.onClick.AddListener(() => PauseButtonCallback());
@@ -34,11 +44,21 @@ public class PlanetIntroManager : MonoBehaviour
     {
         if (!pause)
         {
+            Debug.Log(ratioA);
+
             parent.transform.eulerAngles = new Vector3(0, 1 * iter, 0);
             scrollbar.value = (float)iter / 720;
             iter %= 720;
             iter++;
-        };
+        }
+        if (ratioA != preventRatioA || ratioB != preventRatioB)
+        {
+            preventRatioA = ratioA;
+            preventRatioB = ratioB;
+            planet1.transform.position = new Vector3(ratioA * ratioBias, 0, 0) + centerPosition;
+            planet2.transform.position = new Vector3(-1 * ratioB * ratioBias, 0, 0) + centerPosition;
+        }
+
     }
 
     void ScrollbarCallback(float value)
